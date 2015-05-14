56f47617b5e230c9534b30d85d2123c1fcb5bb39
Maxim Gatilin <gatilin222@gmail.com>
2015-05-14 18:29:39 +0300

    added function for generating project navigation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
-	console.log("hello,world!");
+	pageWidget(['index','town']);
 });
+function pageWidget(pages) {
+	var widgetWrap = $('<div class="widget_wrap"><ul class="widget_list"></ul></div>');
+	widgetWrap.prependTo("body");
+	for (var i = 0; i < pages.length; i++) {
+		$('<li class="widget_item"><a href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').prependTo('.widget_list');
+	}
+}
