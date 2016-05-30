1dd7d6848e9739f1376d8d26bb502c3061122802
gatilin222 <gatilin222@gmail.com>
2016-05-30 22:44:03 +0300

    Animation instruction added

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -130,3 +130,73 @@ $(window).on('resize',function() {
 });
 //*******************************************sliders stuff###
 
+
+//*******************************************animation method
+// create empty array and variable for animation blocks/sections
+var animationArray = [],
+	$animationSection;
+
+
+// on document ready init your animation blocks/sections
+$animationSection = $('.animationSection');
+
+
+// define function for creating animation array 
+function createAnimationArray() {
+	$animationSection.each(function (index, element) {
+		var section = new AnimationSection($(this));
+		animationArray.push(section);
+	});
+}
+
+function AnimationSection(section) {
+	this.section = section;
+	this.animName = section.attr('data-anim-name');
+	this.offsetTop = section.offset().top;
+	this.animationState = false;
+
+	// update values
+	this.update = function () {
+		this.offsetTop = section.offset().top;
+	};
+}
+
+
+// define function to play animation
+function playAnimation(position, version) {
+	if (animationArray.length) {
+		animationArray.forEach(function (item) {
+			if (position > (item.offsetTop - (windowHeight / 2.5) * version) && item.animationState == false) {
+				item.animationState = true;
+
+				if (item.animName == "some_name") {
+					// some stuff to action
+				}
+			}
+		});
+	}
+}
+
+// on window.load invoke function to create animation array and trigger scroll
+createAnimationArray();
+$(window).trigger('scroll');
+
+// on window.resize update your sizes and animation array
+updateSizes();
+animationArray.forEach(function (item) {
+	item.update();
+});
+
+// when pages is scrolling update sizes and play animation
+updateSizes();
+playAnimation($(window).scrollTop(), 1);
+
+// function to update sizes
+function updateSizes() {
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+}
+//*******************************************animation method###
+
+
+
