2d9e41ea6ffde00a76aa6d16a78b0f7303bca5ba
gatilin222 <gatilin222@gmail.com>
2016-07-21 08:25:58 +0300

    Content component added

--- gulpfile.js
+++ gulpfile.js
@@ -185,46 +185,46 @@ gulp.task('cssBuild', function () {
 // });
 
 ////--------------------------------------------If you need svg sprite
-// var svgSprite = require('gulp-svg-sprite'),
-// 	svgmin = require('gulp-svgmin'),
-// 	cheerio = require('gulp-cheerio'),
-// 	replace = require('gulp-replace');
-//
-// gulp.task('svgSpriteBuild', function () {
-// 	return gulp.src(assetsDir + 'i/icons/*.svg')
-// 	// minify svg
-// 		.pipe(svgmin({
-// 			js2svg: {
-// 				pretty: true
-// 			}
-// 		}))
-// 		// remove all fill and style declarations in out shapes
-// 		.pipe(cheerio({
-// 			run: function ($) {
-// 				$('[fill]').removeAttr('fill');
-// 				$('[stroke]').removeAttr('stroke');
-// 				$('[style]').removeAttr('style');
-// 			},
-// 			parserOptions: {xmlMode: true}
-// 		}))
-// 		// cheerio plugin create unnecessary string '&gt;', so replace it.
-// 		.pipe(replace('&gt;', '>'))
-// 		// build svg sprite
-// 		.pipe(svgSprite({
-// 			mode: {
-// 				symbol: {
-// 					sprite: "../sprite.svg",
-// 					render: {
-// 						scss: {
-// 							dest:'../../../sass/_sprite.scss',
-// 							template: assetsDir + "sass/templates/_sprite_template.scss"
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}))
-// 		.pipe(gulp.dest(assetsDir + 'i/sprite/'));
-// });
+var svgSprite = require('gulp-svg-sprite'),
+	svgmin = require('gulp-svgmin'),
+	cheerio = require('gulp-cheerio'),
+	replace = require('gulp-replace');
+
+gulp.task('svgSpriteBuild', function () {
+	return gulp.src(assetsDir + 'i/icons/*.svg')
+	// minify svg
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		// remove all fill, style and stroke declarations in out shapes
+		.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		// cheerio plugin create unnecessary string '&gt;', so replace it.
+		.pipe(replace('&gt;', '>'))
+		// build svg sprite
+		.pipe(svgSprite({
+			mode: {
+				symbol: {
+					sprite: "../sprite.svg",
+					render: {
+						scss: {
+							dest:'../../../sass/_sprite.scss',
+							template: assetsDir + "sass/templates/_sprite_template.scss"
+						}
+					}
+				}
+			}
+		}))
+		.pipe(gulp.dest(assetsDir + 'i/sprite/'));
+});
 
 //testing your build files
 gulp.task('validation', function () {
