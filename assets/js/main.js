6687bbc0d3e1ac8d08199f1a47d73dc0087f2343
gatilin222 <gatilin222@gmail.com>
2016-08-12 10:14:14 +0300

    Add object-fit to template

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,16 @@ function updateSizes() {
 	windowHeight = $(window).height();
 }
 
+if ('objectFit' in document.documentElement.style === false) {
+	document.addEventListener('DOMContentLoaded', function () {
+		Array.prototype.forEach.call(document.querySelectorAll('img[data-object-fit]'), function (image) {
+			(image.runtimeStyle || image.style).background = 'url("' + image.src + '") no-repeat 50%/' + (image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'));
+
+			image.src = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'' + image.width + '\' height=\'' + image.height + '\'%3E%3C/svg%3E';
+		});
+	});
+}
+
 //Functions for development
 function getAllClasses(context, output) {
 	var finalArray = [],
