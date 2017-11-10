427651b6dc9df2dccce5b1fa8ab362cbf6d3e5ff
Evgen <jsnnord@gmail.com>
2017-11-10 16:00:55 +0200

    added new plugins

---
+++ plugins/slick/example.js
@@ -0,0 +1,12 @@
+// Initialization
+$('.container').slick({
+	prevArrow: $('.js_slider_prev'),
+	nextArrow: $('.js_slider_next'),
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	dots: false,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 2000
+});
