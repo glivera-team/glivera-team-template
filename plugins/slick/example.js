74923bcd68864150ecbe687a9a73fc38812a0dfc
Gavrilov Nikolay <n.gavrilov83@gmail.com>
2018-12-11 13:42:05 +0200

    add anim example

--- plugins/slick/example.js
+++ plugins/slick/example.js
@@ -1,5 +1,5 @@
 // Initialization
-$('.container').slick({
+$('.container');.slick({
 	prevArrow: $('.js_slider_prev'),
 	nextArrow: $('.js_slider_next'),
 	infinite: true,
@@ -10,3 +10,31 @@ $('.container').slick({
 	autoplay: true,
 	autoplaySpeed: 2000
 });
+
+// if need resize(example)
+var $slider = $('.container');
+
+// add to updateSize function
+if (windowWidth < mediaPoint1) {
+	if (!$slider.hasClass('slick-slider')) {
+		initSliderFunc();
+	}
+} else {
+	if ($slider.hasClass('slick-initialized')) {
+		$slider.slick("unslick");
+	}
+}
+
+function initSliderFunc() {
+	$slider.slick({
+		prevArrow: $('.js_slider_prev'),
+		nextArrow: $('.js_slider_next'),
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+}
